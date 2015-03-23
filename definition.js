9d7f8795bf5a0d8e075a56897dc697495039d00b
Lesterpig <contact@lesterpig.com>
2015-03-23 13:53:53 +0100

    v0.0.2 : refactor votes
    - More vote information in player list
    - Can cancel vote
    - Victory for mafia if only one villager remaining
    
    Close #1
    Close #2

--- definition.js
+++ definition.js
@@ -17,11 +17,11 @@ module.exports = function() {
   // Metadata
 
   this.name        = "Mafia";
-  this.description = "Une version en ligne du jeu de Dmitry Davidoff";
+  this.version     = "0.0.2";
+  this.description = "Une version en ligne du jeu de Dmitry Davidoff - v" + this.version;
   this.minPlayers  = 4;
   this.maxPlayers  = 40;
   this.opVersion   = ">=0.0.2";
-  this.version     = "0.0.1";
 
   this.css         = ["mafia.css"];
 
@@ -42,8 +42,10 @@ module.exports = function() {
     callback(null);
 
     setTimeout(function() {
+      var nbMafio  = room.gameplay.parameters[0].value;
+      var mafioStr = (nbMafio > 1 ? "mafiosi" : "mafioso"); 
       room.message("<strong><i>Vous vous trouvez dans le village de Balakovo. La Mafia rôde, et menace sérieusement la vie des villageois...</i></strong>");
-      room.message("<strong><i>La police locale pense que " + room.gameplay.parameters[0].value + " mafiosi sont parmis vous. Prenez garde !");
+      room.message("<strong><i>La police locale pense que " + nbMafio + " " + mafioStr + " sont parmis vous. Prenez garde !");
       room.nextStage("mafia");
     }, 100);
 
